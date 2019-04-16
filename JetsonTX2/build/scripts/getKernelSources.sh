62c6685b821089e79ffb2933a2988c3b86c764ba
Yasuo Tanaka <yasuo-tanaka@centryarks.com>
2019-04-16 23:11:03 +0900

    v1.1.5 - Driver source code and build script (L4T 28.2.1)](Support for Jetpack 3.3)

--- JetsonTX2/build/scripts/getKernelSources.sh
+++ JetsonTX2/build/scripts/getKernelSources.sh
@@ -3,11 +3,11 @@ apt-add-repository universe
 apt-get update
 # apt-get install qt5-default pkg-config -y
 cd /usr/src
-wget -N http://developer.download.nvidia.com/embedded/L4T/r28_Release_v2.0/GA/BSP/tx2_sources.tbz2
-echo 'Extracting kernel_src.tbz2 from source release'
-sudo tar -xvf tx2_sources.tbz2 public_release/kernel_src.tbz2
-echo 'Expanding kernel_src.tbz2'
+wget -N https://developer.download.nvidia.com/embedded/L4T/r28_Release_v2.1/public_sources.tbz2
+sudo tar -xvf public_sources.tbz2 public_release/kernel_src.tbz2
 tar -xvf public_release/kernel_src.tbz2
+# Space is tight; get rid of the compressed kernel source
+rm -r public_release
 cd kernel/kernel-4.4
 # zcat /proc/config.gz > .config
 # Ready to configure kernel
